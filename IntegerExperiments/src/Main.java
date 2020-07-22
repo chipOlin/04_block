3ef836d1be91826aeb06042860e3b194ad5524a3
Dmitry <irlitsa.d.p@gmail.com>
2020-07-22 12:11:20 +0300

    1 task complete

--- IntegerExperiments/src/Main.java
+++ IntegerExperiments/src/Main.java
@@ -4,7 +4,8 @@ public class Main
     {
         Container container = new Container();
         container.count += 7843;
-
+		
+		System.out.println(container.count);
     }
 
     public Integer sumDigits(Integer number)
