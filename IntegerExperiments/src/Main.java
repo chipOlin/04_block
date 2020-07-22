21fcbc8c3705b4cd8b42a671c44543aacc0d60cb
Dmitry <irlitsa.d.p@gmail.com>
2020-07-22 18:11:56 +0300

    uncomplete task with star

--- IntegerExperiments/src/Main.java
+++ IntegerExperiments/src/Main.java
@@ -4,12 +4,18 @@ public class Main
     {
         Container container = new Container();
         container.count += 7843;
-		
+		/* 
 		System.out.println("count = " + container.count);
 		
 		System.out.println("sumDigits(12345) = " + sumDigits(12345));
 		System.out.println("sumDigits(10) = " + sumDigits(10));
 		System.out.println("sumDigits(5059191) = " + sumDigits(5059191));
+		*/
+		Integer num  = 123;
+		String s = num.toString();
+		Character c = s.charAt(1);
+		//int x = c.digit(c, 10);
+		System.out.println("c - " + Character.isDigit(c));
     }
 
     public static Integer sumDigits(Integer number)
