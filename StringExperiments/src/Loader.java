490b8e72fec1069707d46364b597e7f632b31c2c
Dmitry <irlitsa.d.p@gmail.com>
2020-09-29 23:37:08 +0300

    complete task 1, StringExperiments

--- StringExperiments/src/Loader.java
+++ StringExperiments/src/Loader.java
@@ -6,15 +6,11 @@ public class Loader
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String regularExpression = "(\\d+)";
-
-        Pattern ptrn = Pattern.compile(regularExpression);
-        Matcher matcher = ptrn.matcher(text);
-
-        if(matcher.find()){
-            System.out.println(matcher.group(0));
-        } else {
-            System.out.println("not found");
+        String[] earn = text.split("\\D+");
+        int totalEarn = 0;
+        for(String e : earn) {
+            if(e.length() > 0) totalEarn += Integer.parseInt(e);
         }
+        System.out.println("Общий заработок составляет: " + totalEarn);
     }
 }
