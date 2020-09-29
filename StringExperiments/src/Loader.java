2519b282c7a6183f0eddf5c573c1618ef908f59c
Dmitry <irlitsa.d.p@gmail.com>
2020-09-29 18:26:43 +0300

    programming 01

--- StringExperiments/src/Loader.java
+++ StringExperiments/src/Loader.java
@@ -1,10 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Loader
 {
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String regularExpression = "(\\d+)";
 
-        System.out.println(text);
+        Pattern ptrn = Pattern.compile(regularExpression);
+        Matcher matcher = ptrn.matcher(text);
+
+        if(matcher.find()){
+            System.out.println(matcher.group(0));
+        } else {
+            System.out.println("not found");
+        }
     }
 }
